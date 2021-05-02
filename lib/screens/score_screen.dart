916b0d43eec304f758969f3ab1c6e150016a16bb
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-05-02 16:10:58 +0700

    implement reset question

--- lib/screens/score_screen.dart
+++ lib/screens/score_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quizionare/screens/welcome_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quizionare/components/button.dart';
+import 'package:quizionare/cubit/questions_cubit.dart';
+import 'package:quizionare/screens/home_screen.dart';
+import 'package:quizionare/styles/style.dart';
 
 class ScoreScreen extends StatelessWidget {
   static const id = 'score-screen';
@@ -11,34 +15,49 @@ class ScoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    final QuestionsCubit _questionCubit =
+        BlocProvider.of<QuestionsCubit>(context);
+
     return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Your Score'),
-            SizedBox(
-              height: 16,
-            ),
-            Text(score.toString()),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
-              onPressed: () {
-                // print(questions.questions[0]);
-                Navigator.pushNamed(context, WelcomeScreen.id);
-              },
-              child: Text(
-                'Back to Home',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+      body: BlocProvider(
+        create: (context) => _questionCubit,
+        child: Container(
+          width: size.width,
+          height: size.height,
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Your Score : ',
+                textAlign: TextAlign.center,
+                style: kTextPrimary.copyWith(
+                  fontSize: 32,
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                score.toString(),
+                textAlign: TextAlign.center,
+                style: kTextPrimary.copyWith(
+                  fontSize: 64,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              ButtonPrimary(
+                text: 'Back to Home',
+                onPressed: () {
+                  context.read<QuestionsCubit>().resetNewQuestions();
+                  Navigator.pushNamed(context, HomeScreen.id);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
