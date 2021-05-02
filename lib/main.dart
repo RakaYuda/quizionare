89057643a64d63f1910785323db40c4978f16733
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-05-02 16:10:02 +0700

    move provider to global

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quizionare/api/repository/api_question_repository.dart';
 import 'package:quizionare/cubit/questions_cubit.dart';
-import 'package:quizionare/models/questions.dart';
 import 'package:quizionare/screens/home_screen.dart';
 import 'package:quizionare/screens/score_screen.dart';
 import 'package:quizionare/screens/welcome_screen.dart';
@@ -12,27 +11,25 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen([]),
-        HomeScreen.id: (context) => HomeScreen(),
-        ScoreScreen.id: (context) => ScoreScreen(
-              score: 0,
-            ),
-      },
-      // home: HomeScreen()
-      home: BlocProvider<QuestionsCubit>(
-        create: (context) =>
-            QuestionsCubit(questionRepository: ApiQuestionRepository()),
-        child: HomeScreen(),
+    return BlocProvider<QuestionsCubit>(
+      create: (context) =>
+          QuestionsCubit(questionRepository: ApiQuestionRepository()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen([]),
+          HomeScreen.id: (context) => HomeScreen(),
+          ScoreScreen.id: (context) => ScoreScreen(
+                score: 0,
+              ),
+        },
+        home: HomeScreen(),
       ),
     );
   }
