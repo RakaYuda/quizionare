5c03eb0600096593abda3c3f387041ca3e9aff33
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-05-07 20:09:06 +0700

    add lottie animation for score

--- lib/screens/score_screen.dart
+++ lib/screens/score_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
 import 'package:quizionare/components/button.dart';
 import 'package:quizionare/cubit/questions_cubit.dart';
 import 'package:quizionare/screens/home_screen.dart';
@@ -21,43 +22,55 @@ class ScoreScreen extends StatelessWidget {
     return Scaffold(
       body: BlocProvider(
         create: (context) => _questionCubit,
-        child: Container(
-          width: size.width,
-          height: size.height,
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Your Score : ',
-                textAlign: TextAlign.center,
-                style: kTextPrimary.copyWith(
-                  fontSize: 32,
-                ),
+        child: Stack(
+          children: [
+            Center(
+              child: Lottie.asset(
+                'animation/confetti.json',
+                repeat: true,
+                reverse: false,
+                animate: true,
               ),
-              SizedBox(
-                height: 32,
+            ),
+            Container(
+              width: size.width,
+              height: size.height,
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Your Score : ',
+                    textAlign: TextAlign.center,
+                    style: kTextPrimary.copyWith(
+                      fontSize: 32,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    score.toString(),
+                    textAlign: TextAlign.center,
+                    style: kTextPrimary.copyWith(
+                      fontSize: 64,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 64,
+                  ),
+                  ButtonPrimary(
+                    text: 'Back to Home',
+                    onPressed: () {
+                      context.read<QuestionsCubit>().resetNewQuestions();
+                      Navigator.pushNamed(context, HomeScreen.id);
+                    },
+                  ),
+                ],
               ),
-              Text(
-                score.toString(),
-                textAlign: TextAlign.center,
-                style: kTextPrimary.copyWith(
-                  fontSize: 64,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              ButtonPrimary(
-                text: 'Back to Home',
-                onPressed: () {
-                  context.read<QuestionsCubit>().resetNewQuestions();
-                  Navigator.pushNamed(context, HomeScreen.id);
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
